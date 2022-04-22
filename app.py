fc2903adf1a3db25fad914e96e0ce0b4f6722302
artur3456 <a01376086@itesm.mx>
2022-04-21 20:39:33 -0500

    Se agrega funcionalidad y métodos de la base de datos MongoDB

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 #from crypt import methods
 from datetime import datetime
 import email
+from re import T
 from flask import Flask, redirect, render_template, request, session, url_for
 import datetime
+import pymongo
 
 # FlASK
 #############################################################
@@ -12,6 +14,16 @@ app.permanent_session_lifetime = datetime.timedelta(days=365)
 #Con esta llave, la sesión queda encriptada.
 app.secret_key = "super secret key"
 #############################################################
+
+# MONGODB
+#############################################################
+mongodb_key = "mongodb+srv://desarrollowebuser:desarrollowebpassword@cluster0.dfh7g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+client = pymongo.MongoClient(
+    mongodb_key, tls=True, tlsAllowInvalidCertificates=True)
+db = client.Escuela
+cuentas = db.alumno
+#############################################################
+
 
 
 @app.route('/')
@@ -81,4 +93,71 @@ def logout():
     if "email" in session:
         session.clear()
         return redirect(url_for("home"))
+
+
+@app.route("/usuarios")
+def usuarios():
+    cursor = cuentas.find({})
+    users = []
+    for doc in cursor:
+        users.append(doc)
+    return render_template("/usuarios.html", data=users)
+
+@app.route("/insert", methods = ["POST"])
+def insertUsers():
+    user = {
+        "matricula": request.form["matricula"],
+        "nombre": request.form["nombre"],
+        "correo": request.form["correo"],
+        "contrasena": request.form["contrasena"]
+    }
+
+    try:
+        cuentas.insert_one(user)
+        return redirect(url_for("usuarios"))
+    except Exception as e:
+        return "<p> El servicio no está disponible =>: %s %s." % type(e), e
     
+
+@app.route("/find_one/<matricula>")
+def find_one(matricula):
+    try:
+        user = cuentas.find_one({ "matricula": (matricula) })
+        if user == None:
+            return "<p> La matricula %s no existe en Monguito</p>" % (matricula)
+        else:
+            return "<p> Encontramos: %s </p>" % (user)
+    except Exception as e:
+        return " %s " % e
+
+
+@app.route("/delete_one/<matricula>")
+def delete_one(matricula):
+    try:
+        user = cuentas.delete_one({ "matricula": (matricula) })
+        if user.deleted_count == 0: # O sea que no borró nada
+            return "<p> La matricula %s no existe en Monguito</p>" % (matricula)
+        else:
+            return redirect(url_for("usuarios"))
+    except Exception as e:
+        return " %s " % e
+
+
+@app.route("/update", methods=["POST"])
+def update():
+    try:
+        filter = {"matricula": request.form["matricula"]} #Primero crea un filtro de busqueda.
+        user = {"$set": {
+            "nombre": request.form["nombre"],
+            "constrasena": request.form["contrasena"]
+        }}
+        cuentas.update_one(filter, user)
+        return redirect(url_for("usuarios"))
+
+    except Exception as e:
+        return " %s " % e
+
+
+@app.route("/create")
+def create():
+    return render_template("createForm.html")
